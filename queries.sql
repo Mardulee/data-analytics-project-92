be5ce40c4b5f9ea1c4ecf6e9ee6732fc16173256
Mardulee <mari.dulepova@gmail.com>
2025-04-10 11:41:10 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -51,3 +51,66 @@ ORDER BY
   EXTRACT(ISODOW FROM s.sale_date),
   seller;
 -- проект 1_задание 5_часть 3 : Запрос конкатенирует имя и фамилию продавца в 1 записи, выводит к ним день недели продажи в формате названия дня недели и выводит доход по купленным товарам, округленный в меньшую сторону и сортирует записи по продавцу, дню недели, номеру дня недели 
+
+
+
+WITH categorized_users AS (
+SELECT customer_id,
+CASE
+WHEN age BETWEEN 16 AND 25 THEN '16-25'
+WHEN age BETWEEN 26 AND 40 THEN '26-40'
+WHEN age > 40 THEN '40+'
+END AS age_category
+FROM customers
+WHERE age >= 16
+)
+SELECT age_category, COUNT(distinct (customer_id)) AS age_count
+FROM categorized_users
+GROUP BY age_category
+ORDER BY
+CASE age_category
+WHEN '16-25' THEN 1
+WHEN '26-40' THEN 2
+WHEN '40+' THEN 3
+ELSE 4
+END;
+-- проект 1_задание 6_часть 1 : Вспомогательный подзапрос классифицирует всех покупателей в БД customers по возрастным категориям, далее выводим таблицу с кол-вом уникальных покупателей по каждой возрастной категории
+
+
+SELECT
+TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+COUNT(DISTINCT s.customer_id) AS total_customers,
+ROUND(SUM(p.price * s.quantity),0) AS income
+FROM sales s
+JOIN products p ON s.product_id = p.product_id  
+GROUP BY TO_CHAR(s.sale_date, 'YYYY-MM')
+ORDER BY selling_month ASC;
+-- проект 1_задание 6_часть 2 : Запрос выводит месяцы продаж с кол-вом уникальных покупателей и округленной общей выручкой от них
+
+
+WITH tab1 AS
+    (
+    SELECT CONCAT(c.first_name, ' ', c.last_name) AS customer, min(sale_date) AS sale_date, 
+    sum(price * quantity) AS income
+    FROM customers c 
+    JOIN sales s ON c.customer_id = s.customer_id
+    JOIN products p ON s.product_id = p.product_id
+    GROUP BY CONCAT(c.first_name, ' ', c.last_name)
+    HAVING sum(price * quantity) = 0
+    ), 
+    tab2 AS 
+    (
+    SELECT CONCAT(c.first_name, ' ', c.last_name) AS customer, min(sale_date) AS sale_date, 
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+    FROM sales s
+    JOIN customers c ON s.customer_id = c.customer_id
+    JOIN employees e  ON e.employee_id = s.sales_person_id
+    GROUP BY CONCAT(c.first_name, ' ', c.last_name), 
+             CONCAT(e.first_name, ' ', e.last_name)
+    )
+    SELECT tab1.customer, tab1.sale_date, tab2.seller
+    FROM tab1
+    INNER JOIN tab2 ON tab1.customer = tab2.customer AND tab1.sale_date = tab2.sale_date
+    GROUP BY tab1.customer, tab1.sale_date, tab2.seller
+    ORDER BY customer;
+-- проект 1_задание 6_часть 3 : Подзапрос 1 выводит сконкатенированную строку с именем и фамилией покупателя, его датой первой покупки с суммой затрат, при этом сумма затрат = 0. Подзапрос 2 выводит сконкатенированную строку с именем и фамилией покупателя, его датой первой покупки и сконкатенированную строку с именем и фамилией продавца. Итоговый запрос выводит ФИ покупателя, дату первой покупки и ФИ продавца с сортировкой по покупателю.
